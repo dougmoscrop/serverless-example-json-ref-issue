e8acfb5f75c226e19ce86f7755e581cdfe8534d7
Doug Moscrop <doug.moscrop@gmail.com>
2018-06-06 15:03:58 -0400

    use deepEqual for proper valid comparison

--- check-template.js
+++ check-template.js
@@ -3,11 +3,13 @@
 const fs = require('fs');
 const path = require('path');
 
+const deepEqual = require('deep-equal');
+
 const templatePath = path.join(__dirname, '.serverless', 'cloudformation-template-update-stack.json');
 const contents = fs.readFileSync(templatePath);
 const template = JSON.parse(contents);
 
-if (template.Resources.bar.Properties.test === template.Resources.foo.Properties.test) {
+if (deepEqual(template.Resources.bar.Properties.test, template.Resources.foo.Properties.test)) {
   console.log('Looks good!');
 } else {
   console.error('json-cycle produced invalid CloudFormation syntax!');
